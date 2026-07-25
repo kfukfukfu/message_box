a9ede3df52cabe6ab9d808672f7758b9967b2ec0
kfukfukfu <kfu53825@gmail.com>
2026-07-25 18:24:32 +0900

    commit

--- app.py
+++ app.py
@@ -1,7 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, flash, redirect, render_template, request
+from werkzeug.security import generate_password_hash
 from config import User
 
 app = Flask(__name__)
+app.secret_key = "secret"
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # データの検証
+        if not request.form["name"] or not request.form["password"] or not request.form["email"]:
+            flash("未入力の項目があります。")
+            return redirect(request.url)
+        if User.select().where(User.name == request.form["name"]):
+            flash("その名前はすでに使われています。")
+
+            return redirect(request.url)
+        if User.select().where(User.email == request.form["email"]):
+            flash("そのメールアドレスはすでに使われています。")
+            return redirect(request.url)
+
+        # ユーザー登録
+        User.create(
+            name=request.form["name"],
+            email=request.form["email"],
+            password=generate_password_hash(request.form["password"]),
+        )
+        return render_template("index.html")
+
+    return render_template("register.html")
 
 
 @app.route("/")
@@ -10,4 +38,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000, debug=True)
+    app.run(host="127.0.0.1", port=8000)
